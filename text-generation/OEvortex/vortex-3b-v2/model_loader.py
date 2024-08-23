597a4222b88e1a8dae566a9721722f8aafd90a59
Aaron Vogler <aaronvogler@gmail.com>
2024-08-23 15:53:58 -0400

    Create files for text-generation, OEvortex/vortex-3b-v2, image version: 0.0.1

---
+++ text-generation/OEvortex/vortex-3b-v2/model_loader.py
@@ -0,0 +1,36 @@
+from optimum.pipelines import pipeline
+from environment import MODEL_ID, TASK
+from streamer import SingleTokenStreamer
+
+
+print("Loading model...")
+
+pipe = pipeline(
+    TASK,
+    model=MODEL_ID,
+    accelerator="ort",
+  ### params ###
+    device_map = "auto",
+    trust_remote_code = True
+)
+
+print("Model loaded")
+
+if TASK in ["text-generation"]:
+    streamer = SingleTokenStreamer(
+        tokenizer=pipe.tokenizer, skip_prompt=False, skip_special_tokens=True
+    )
+
+    pipe._forward_params = {**pipe._forward_params, "streamer": streamer}
+
+
+def load_model():
+    # instructions: do not modify this function
+    # export the model loaded into global memory
+    global pipe
+
+    return pipe
+
+
+if __name__ == "__main__":
+    load_model()
