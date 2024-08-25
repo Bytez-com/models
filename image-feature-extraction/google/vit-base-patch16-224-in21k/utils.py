294555a172dd53564f7d27b851ac06ee19e3684e
Aaron Vogler <aaronvogler@gmail.com>
2024-08-25 19:39:36 -0400

    Create files for image-feature-extraction, google/vit-base-patch16-224-in21k, image version: 0.0.1

---
+++ image-feature-extraction/google/vit-base-patch16-224-in21k/utils.py
@@ -0,0 +1,49 @@
+import threading
+from model import model_run
+from model import pipe
+from streamer import SingleTokenStreamer
+
+
+def model_run_generator(user_input, params: dict):
+    streamer: SingleTokenStreamer = pipe._forward_params.get("streamer")
+
+    # add the streamer to the pipe if it isn't already there
+    if not streamer:
+        streamer = SingleTokenStreamer(
+            tokenizer=pipe.tokenizer, skip_prompt=False, skip_special_tokens=True
+        )
+
+        pipe._forward_params = {**pipe._forward_params, "streamer": streamer}
+
+    # always clear the stream before a request is made
+    # if we introduce concurrency, then we cannot get away with a singleton
+    streamer.reset()
+
+    def model_run_thread():
+        model_run(user_input, params=params)
+
+    try:
+        # run the model in its own thread, it will magically add
+        # its streamed output to the streamer object's queue
+        thread = threading.Thread(target=model_run_thread)
+
+        # this is our generator "hook" to the model
+        def output_generator():
+            for val in streamer:
+                val: str
+
+                yield val
+
+            # cleanup the thread, needs to be here because flask doesn't have a way of cleaning this up otherwise
+            thread.join()
+
+        # run the model
+        thread.start()
+
+        # return the generator
+        return output_generator
+
+    except Exception as exception:
+        # always cleanup the thread on failure, no finally block because normal cleanup has to happen in the generator
+        thread.join()
+        raise exception
