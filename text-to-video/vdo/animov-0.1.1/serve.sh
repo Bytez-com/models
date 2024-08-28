5807ba49318b5b75fed8afb28c86c18118a200b6
Aaron Vogler <aaronvogler@gmail.com>
2024-08-27 23:31:02 -0400

    Create files for text-to-video, vdo/animov-0.1.1, image version: 0.0.1

---
+++ text-to-video/vdo/animov-0.1.1/serve.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+apt-get update && apt-get install -y libgl1 libglib2.0-0
+
+pip install -r /server/requirements.txt
+
+# Use exec to replace the shell with gunicorn, passing environment variables directly into the command
+exec gunicorn --bind 0.0.0.0:${PORT:-8000} --timeout 31536000 server:app
