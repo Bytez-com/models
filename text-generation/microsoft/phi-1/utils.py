2b586a619d0582fe00ec9434f8e7a038e1052bed
inf3rnus <root@model-containerize-group-30b3.c.bytez-web-services.internal>
2024-09-03 21:32:01 +0000

    Create files for text-generation, microsoft/phi-1, image version: 0.0.1

--- text-generation/microsoft/phi-1/utils.py
+++ text-generation/microsoft/phi-1/utils.py
@@ -1,20 +1,44 @@
+import time
 import threading
-from model_loader import streamer
 from model import model_run
+from model import pipe
+from streamer import SingleTokenStreamer
+import numpy as np
 
 
 def model_run_generator(user_input, params: dict):
-    # always have a fresh stream before a request is made
+    streamer: SingleTokenStreamer = pipe._forward_params.get("streamer")
+
+    # add the streamer to the pipe if it isn't already there
+    if not streamer:
+        streamer = SingleTokenStreamer(
+            tokenizer=pipe.tokenizer, skip_prompt=False, skip_special_tokens=True
+        )
+
+        pipe._forward_params = {**pipe._forward_params, "streamer": streamer}
+
+    # always clear the stream before a request is made
     # if we introduce concurrency, then we cannot get away with a singleton
     streamer.reset()
 
     def model_run_thread():
-        model_run(user_input, params=params)
+        try:
+            model_run(user_input, params=params)
+        except Exception as exception:
+            streamer.text_queue.put(
+                'INTERNAL_BYTEZ_ERROR: arg "stream" was likely passed to a model that does not support streaming.'
+            )
+            # make sure the generator stops on an exception, otherwise the request will hang and never complete
+            streamer.end()
+            raise exception
 
     try:
         # run the model in its own thread, it will magically add
         # its streamed output to the streamer object's queue
         thread = threading.Thread(target=model_run_thread)
+
+        # run the model
+        thread.start()
 
         # this is our generator "hook" to the model
         def output_generator():
@@ -26,9 +50,6 @@ def model_run_generator(user_input, params: dict):
             # cleanup the thread, needs to be here because flask doesn't have a way of cleaning this up otherwise
             thread.join()
 
-        # run the model
-        thread.start()
-
         # return the generator
         return output_generator
 
@@ -36,3 +57,28 @@ def model_run_generator(user_input, params: dict):
         # always cleanup the thread on failure, no finally block because normal cleanup has to happen in the generator
         thread.join()
         raise exception
+
+
+def convert_np(obj):
+    if isinstance(obj, np.ndarray):
+        return obj.tolist()
+    elif isinstance(obj, (np.integer, np.floating)):
+        return obj.item()
+    elif isinstance(obj, dict):
+        return {key: convert_np(value) for key, value in obj.items()}
+    elif isinstance(obj, list):
+        return [convert_np(element) for element in obj]
+    else:
+        return obj
+
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(f"Function '{func.__name__}' took {elapsed_time:.4f} seconds to execute.")
+        return result
+
+    return wrapper
