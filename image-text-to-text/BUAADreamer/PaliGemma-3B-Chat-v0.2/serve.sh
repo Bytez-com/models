595448205242018478fb921b33b5f78f0b1ac2e7
Aaron V <aaron@bytez.com>
2025-01-16 22:05:26 +0000

    Create files for image-text-to-text, BUAADreamer/PaliGemma-3B-Chat-v0.2, image version: 0.0.1

--- image-text-to-text/BUAADreamer/PaliGemma-3B-Chat-v0.2/serve.sh
+++ image-text-to-text/BUAADreamer/PaliGemma-3B-Chat-v0.2/serve.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 pip install -r /server/requirements.txt
+# TODO remove this once all images are updated to include flask async pre installed
 pip install flask[async]
 
 # Use exec to replace the shell with gunicorn, passing environment variables directly into the command
