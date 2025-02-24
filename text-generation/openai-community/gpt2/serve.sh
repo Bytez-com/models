2be751b4702e7eeb1a5b4e6e1e6d59be35f07faa
Aaron V <inf3rnus@users.noreply.github.com>
2025-02-24 15:47:22 -0500

    Update serve.sh

--- text-generation/openai-community/gpt2/serve.sh
+++ text-generation/openai-community/gpt2/serve.sh
@@ -3,5 +3,7 @@ pip install -r /server/requirements.txt
 # TODO remove this once all images are updated to include flask async pre installed
 pip install flask[async]
 
+pip install pynvml
+
 # Use exec to replace the shell with gunicorn, passing environment variables directly into the command
 exec gunicorn --bind 0.0.0.0:${PORT:-8000} --threads 32 --timeout 31536000 server:app
