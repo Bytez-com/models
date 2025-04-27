f05cc8cd545c30785e5a4559be426c4a87e5e0a0
inf3rnus <root@model-containerize-group-5lxb.c.bytez-web-services.internal>
2025-04-27 23:52:11 +0000

    Create files for document-question-answering, Chan-yeong/donut-sroie-company-sample-demo, image version: 0.0.1

---
+++ document-question-answering/Chan-yeong/donut-sroie-company-sample-demo/serve.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+python3 /server/download_bytez_repo.py
+
+apt update
+
+apt install tesseract-ocr tesseract-ocr-eng libtesseract-dev -y
+
+pip install -r /server/requirements.txt
+
+# Use exec to replace the shell with gunicorn, passing environment variables directly into the command
+exec gunicorn --bind 0.0.0.0:${PORT:-8000} --timeout 31536000 server:app
