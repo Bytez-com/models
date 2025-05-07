d27f1468bc2233ea47198fba8b81e63435001820
inf3rnus <root@model-containerize-group-58xl.c.bytez-web-services.internal>
2025-05-07 10:27:12 +0000

    Create files for text-to-speech, walterheart/handler, image version: 0.0.1

---
+++ text-to-speech/walterheart/handler/run_endpoint_handler.py
@@ -0,0 +1,31 @@
+import io
+import base64
+from flask import jsonify
+from scipy.io.wavfile import write as wav_write
+from model import model_run
+
+
+def run_endpoint_handler(request):
+    params = request.json.get("params", {})
+    user_input = request.json["text"]
+
+    # model inference
+    model_output = model_run(user_input, params)
+
+    # Create an in-memory buffer
+    buffer = io.BytesIO()
+
+    audio = model_output["audio"].squeeze()
+
+    # Write the WAV file to the buffer
+    wav_write(buffer, rate=model_output["sampling_rate"], data=audio)
+
+    # Get the WAV data from the buffer
+    buffer.seek(0)
+    wav_data = buffer.read()
+
+    # Encode the WAV data to base64
+    wav_base64 = base64.b64encode(wav_data).decode("utf-8")
+
+    # Return response with base64 encoded audio
+    return jsonify({"output_wav": wav_base64})
