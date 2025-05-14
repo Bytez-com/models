349541a2c495445e5dff00eba5bf5f06960a74de
Aaron V <inf3rnus@users.noreply.github.com>
2025-05-14 17:42:57 -0400

    Update run_endpoint_handler.py

--- text-to-video/ali-vilab/text-to-video-ms-1.7b/run_endpoint_handler.py
+++ text-to-video/ali-vilab/text-to-video-ms-1.7b/run_endpoint_handler.py
@@ -1,8 +1,19 @@
-import os
 import base64
 from flask import jsonify
 from model import model_run
-from diffusers.utils import export_to_video
+import imageio
+
+
+def export_to_video(frames, output_video_path):
+    writer = imageio.get_writer(
+        output_video_path,
+        fps=8,
+        codec="libx264",  # ✅ Chrome-compatible
+        ffmpeg_params=["-pix_fmt", "yuv420p"],  # ✅ required for browser playback
+    )
+    for frame in frames:
+        writer.append_data(frame)
+    writer.close()
 
 
 def run_endpoint_handler(request):
@@ -24,8 +35,18 @@ def run_endpoint_handler(request):
         with open(tmp_file_path, "rb") as video_file:
             video_base64 = base64.b64encode(video_file.read()).decode("utf-8")
 
+        print(f"Output b64 length is: {len(video_base64)}")
+
+        with open("/tmp/video-decoded.mp4", "wb") as video_file:
+            video_binary = base64.b64decode(video_base64)
+
+            print(f"Output binary length is: {len(video_binary)}")
+
+            video_file.write(video_binary)
+
         # Return response with base64 encoded video
         return jsonify({"output_mp4": video_base64})
     finally:
         # Ensure the temporary file is deleted
-        os.remove(tmp_file_path)
+        # os.remove(tmp_file_path)
+        pass
