d48cfeaa6414d47b12c1f4199e40b3b652a92216
inf3rnus <root@model-containerize-group-9qbv.c.bytez-web-services.internal>
2025-06-03 22:06:03 +0000

    Create files for text-generation, databricks/dolly-v2-7b, image version: 0.0.1

--- text-generation/databricks/dolly-v2-7b/model_loader.py
+++ text-generation/databricks/dolly-v2-7b/model_loader.py
@@ -1,28 +1,72 @@
+from collections import OrderedDict
 from transformers import pipeline
-from environment import MODEL_ID, TASK
+from environment import MODEL_ID, TASK, DEVICE, MODEL_LOADING_KWARGS
+from validate_pipe import validate_pipe
 
-print("Loading model...")
 
-pipe = pipeline(
-    TASK,
-    model=MODEL_ID,
+# construct as a set to dedupe, then turn into list
+FALL_BACK_DEVICES = [
+    "auto",
+    # if the device was specified, i.e. cuda for instances, then if auto fails, this will run
+    DEVICE,
+    # always fallback to cpu worst case scenario
+    "cpu",
+]
+
+# Use OrderedDict to maintain order while deduplicating
+DEVICES = list(OrderedDict.fromkeys(FALL_BACK_DEVICES))
+DEVICES_NO_AUTO = DEVICES[1:]
+
+
+DEFAULT_KWARGS = {
     ### params ###
-    device_map = "auto",
-    trust_remote_code = True
-)
+    "task": TASK,
+    "model": MODEL_ID,
+    "trust_remote_code": True
+}
+
+
+def try_loading():
+    # we'll try loading on "device_map" first, then "device". This is to ensure a model at least runs on the CPU if
+    # it fails to load on cuda on an instance
+    loading_methods = [
+        ["device_map", DEVICES],
+        # NOTE device is special, it doesn't support 'auto'
+        ["device", DEVICES_NO_AUTO],
+    ]
+
+    collected_exception = None
+
+    for loading_method, devices in loading_methods:
+        for device in devices:
+            try:
+                print(
+                    f"Attempting to load model via '{loading_method}' with device: ",
+                    device,
+                )
+
+                kwargs = {**DEFAULT_KWARGS, **MODEL_LOADING_KWARGS}
+
+                # set the kwargs to specifically have the loading method and the device
+                kwargs.setdefault(loading_method, device)
+
+                pipe = pipeline(**kwargs)
+
+                print(f"Loaded model via '{loading_method}' on device: ", device)
+
+                return pipe
+            except Exception as exception:
+                collected_exception = exception
+
+    raise collected_exception
+
+
+pipe = try_loading()
+
+# this does a double check for things that should be present, e.g. tokenizers, image_processors, etc.
+validate_pipe(pipe)
 
 print("Model loaded")
-
-
-
-if TASK in ["text-generation"]:
-    from streamer import SingleTokenStreamer
-    
-    streamer = SingleTokenStreamer(
-        tokenizer=pipe.tokenizer, skip_prompt=False, skip_special_tokens=True
-    )
-
-    pipe._forward_params = {**pipe._forward_params, "streamer": streamer}
 
 
 def load_model():
