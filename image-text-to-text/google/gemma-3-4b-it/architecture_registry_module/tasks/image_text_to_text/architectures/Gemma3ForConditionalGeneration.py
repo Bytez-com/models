256c418a128ebfd1cdd009fe26f8be62f6ee8ce9
Aaron V <inf3rnus@users.noreply.github.com>
2025-06-06 14:54:27 -0400

    Update Gemma3ForConditionalGeneration.py

--- image-text-to-text/google/gemma-3-4b-it/architecture_registry_module/tasks/image_text_to_text/architectures/Gemma3ForConditionalGeneration.py
+++ image-text-to-text/google/gemma-3-4b-it/architecture_registry_module/tasks/image_text_to_text/architectures/Gemma3ForConditionalGeneration.py
@@ -7,8 +7,34 @@ from architecture_registry_module.tasks.image_text_to_text.model_entity import (
 
 @dataclass
 class Gemma3ForConditionalGeneration(ImageTextToTextModelEntity):
-    pass
-    
+    def generate(self, text, images, videos, **kwargs):
+        kwargs = {**{"generate_kwargs": {"streamer": kwargs.get("streamer")}, **kwargs}}
+
+        if videos:
+            kwargs["videos"] = videos
+
+        # NOTE if images is an empty array, set it to none, it will fail on some models otherwise
+        if not images:
+            images = None
+
+        # Save the original
+        og_call = self.pipe.processor.__class__.__call__
+
+        # Define the patch
+        def patched_processor_call(self, *args, **kwargs):
+            images = kwargs.get("images", None)
+            if not images:
+                kwargs["images"] = None
+            return og_call(self, *args, **kwargs)
+
+        # Apply it
+        self.pipe.processor.__class__.__call__ = patched_processor_call
+
+        output = self.pipe(text=text, images=images, **kwargs)
+
+        generated_text = output[0]["generated_text"]
+
+        return generated_text
 
 
 # universal stub used by the model loader
