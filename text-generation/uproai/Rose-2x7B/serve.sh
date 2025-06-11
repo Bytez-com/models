6250f66b59cdc65d90c23eb39e8cf2ffc6599633
inf3rnus <root@model-containerize-group-3lzq.c.bytez-web-services.internal>
2025-06-11 08:39:18 +0000

    Create files for text-generation, uproai/Rose-2x7B, image version: 0.0.5

--- text-generation/uproai/Rose-2x7B/serve.sh
+++ text-generation/uproai/Rose-2x7B/serve.sh
@@ -3,8 +3,5 @@ pip install -r /server/requirements.txt
 # TODO remove this once all images are updated to include flask async pre installed
 pip install flask[async]
 
-# allows us to track model download progress on GPUs
-pip install pynvml
-
 # Use exec to replace the shell with gunicorn, passing environment variables directly into the command
 exec gunicorn --bind 0.0.0.0:${PORT:-8000} --threads 32 --timeout 31536000 server:app
