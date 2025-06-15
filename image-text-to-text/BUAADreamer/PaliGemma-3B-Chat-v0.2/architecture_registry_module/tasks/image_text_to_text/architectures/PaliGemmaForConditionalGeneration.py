8dd89cada1975ff86627c12c2218e654d07cb928
inf3rnus <root@model-containerize-group-pwhr.c.bytez-web-services.internal>
2025-06-15 14:27:33 +0000

    Create files for image-text-to-text, BUAADreamer/PaliGemma-3B-Chat-v0.2, image version: 0.0.5

--- image-text-to-text/BUAADreamer/PaliGemma-3B-Chat-v0.2/architecture_registry_module/tasks/image_text_to_text/architectures/PaliGemmaForConditionalGeneration.py
+++ image-text-to-text/BUAADreamer/PaliGemma-3B-Chat-v0.2/architecture_registry_module/tasks/image_text_to_text/architectures/PaliGemmaForConditionalGeneration.py
@@ -6,15 +6,32 @@ from architecture_registry_module.tasks.image_text_to_text.model_entity import (
 
 
 @dataclass
-class PaliGemmaModelEntity(ImageTextToTextModelEntity):
-    def run_inference_default(self, text, images, videos=None, **kwargs):
-        output = self.generate(text, images, videos, **kwargs)
+class Gemma3ForConditionalGeneration(ImageTextToTextModelEntity):
+    def generate(self, text, images, videos, **kwargs):
+        kwargs = {**{"generate_kwargs": {"streamer": kwargs.get("streamer")}, **kwargs}}
 
-        formatted_output = output[len(text):]
+        if videos:
+            kwargs["videos"] = videos
 
-        return formatted_output
-    
+        # Save the original
+        og_call = self.pipe.processor.__class__.__call__
+
+        # Define the patch
+        def patched_processor_call(self, *args, **kwargs):
+            images = kwargs.get("images", None)
+            if not images:
+                kwargs["images"] = None
+            return og_call(self, *args, **kwargs)
+
+        # Apply it
+        self.pipe.processor.__class__.__call__ = patched_processor_call
+
+        output = self.pipe(text=text, images=images, **kwargs)
+
+        generated_text = output[0]["generated_text"]
+
+        return generated_text
 
 
 # universal stub used by the model loader
-model_cls = PaliGemmaModelEntity
+model_cls = Gemma3ForConditionalGeneration
